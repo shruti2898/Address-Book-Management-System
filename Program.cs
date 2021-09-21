f32e704abacea6d7c14b2af72208522bc9adac16
Shruti <shrutisablaniya93@gmail.com>
2021-09-21 15:45:40 +0530

    Address Book Management System Main Class

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book\n");
+
+            //UC1_CreateContact.CreateContact();
+            UC2_AddNewContact.AddNewContacts();
+            //UC3_UpdateExistingContact.UpdateContact();
+            //UC4_DeleteContact.ContactDelete();
+            Console.ReadKey();
+        }
+    }
+}
